3ea89690dee9a5a25f0387948a291f1b56eb4ebe
morrisonsr <morrisonsr@g.cofc.edu>
2026-04-25 22:34:22 -0400

    fixed part 2 graphs

--- Run_ServiceQueue_Renege.m
+++ Run_ServiceQueue_Renege.m
@@ -113,12 +113,12 @@ end
 % L Simulated
 NumInSystem = vertcat(NumInSystemSamples{:});
 meanNumInSystemSamples = mean(NumInSystem);
-fprintf("Mean number in system: %f\n", meanNumInSystemSamples); %[output:524f9e70]
+fprintf("Mean number in system: %f\n", meanNumInSystemSamples); %[output:8dc7a1da]
 
 % L_q Simulated
 NumInWaiting = vertcat(NumInWaitingSamples{:});
 meanNumInWaitingSamples = mean(NumInWaiting);
-fprintf("Mean number waiting in system: %f\n", meanNumInWaitingSamples); %[output:207e5879]
+fprintf("Mean number waiting in system: %f\n", meanNumInWaitingSamples); %[output:042b9543]
 %[text] ### Option two: Map a function over the cell array of ServiceQueue objects.
 %[text] The `@(q) ...` expression is shorthand for a function that takes a ServiceQueue as input, names it `q`, and computes the sum of two columns from its log.  The `cellfun` function applies that function to each item in `QSamples`. The option `UniformOutput=false` tells `cellfun` to produce a cell array rather than a numerical array.
 %NumInSystemSamples = cellfun( ...
@@ -395,10 +395,10 @@ exportgraphics(fig, PictureFolder + filesep + "ServiceTime_histogram.pdf"); %[ou
 %[output:2a5b77a2]
 %   data: {"dataType":"text","outputData":{"text":"10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200 210 220 230 240 250 260 270 280 290 300 310 320 330 340 350 360 370 380 390 400 410 420 430 440 450 460 470 480 490 500 ","truncated":false}}
 %---
-%[output:524f9e70]
+%[output:8dc7a1da]
 %   data: {"dataType":"text","outputData":{"text":"Mean number in system: 1.273304\n","truncated":false}}
 %---
-%[output:207e5879]
+%[output:042b9543]
 %   data: {"dataType":"text","outputData":{"text":"Mean number waiting in system: 0.694962\n","truncated":false}}
 %---
 %[output:443132b8]
